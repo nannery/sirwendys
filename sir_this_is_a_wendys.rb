1f87b470b4d87c77bf7c969f3dea9cca4e72ad6c
David Nanry <dnanry>
2018-11-04 13:05:19 -0600

    only reply when havent already replied

--- sir_this_is_a_wendys.rb
+++ sir_this_is_a_wendys.rb
@@ -9,23 +9,22 @@ CLIENT = Twitter::REST::Client.new do |c|
 end
 
 CUSTOMER = 'realDonaldTrump'
+COMPANY  = 'SirWendys'
 COMMENT  = "@#{CUSTOMER} Excuse me, sir, this is a Wendy's"
 
-def last_checked_time
-  Time.at(Time.now.to_i - 60)
-end
+def let_the_customer_know_where_he_or_she_is
+  customer_latest_tweet = CLIENT.user_timeline(CUSTOMER).first
+  our_latest_tweet      = CLIENT.user_timeline(COMPANY).first
 
-def does_the_customer_know_where_he_or_she_is?
-  latest_tweet = CLIENT.user_timeline(CUSTOMER).first
-  #if latest_tweet.created_at > last_checked_time
-    CLIENT.update(COMMENT, in_reply_to_status_id: latest_tweet.id)
-  #end
+  if our_latest_tweet.in_reply_to_status_id != customer_latest_tweet.id
+    CLIENT.update(COMMENT, in_reply_to_status_id: customer_latest_tweet.id)
+  end
 end
 
 Cuba.define do
   on get do
     on root do
-      does_the_customer_know_where_he_or_she_is?
+      let_the_customer_know_where_he_or_she_is
       res.write 'Sir, this is a Wendy\'s'
     end
   end
